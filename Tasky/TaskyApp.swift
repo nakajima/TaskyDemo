27e37a0622700f8a19afd8cf5c0f446f7feb061b
Pat Nakajima <patnakajima@gmail.com>
2024-04-23 11:56:20 -0700

    Initial Commit

---
+++ Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Pat Nakajima on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
